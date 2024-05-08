efd9cef4cb13813ea5d1f25c8ba8de7b7b21d0a2
hwan2272 <hwan2230@naver.com>
2024-05-08 18:02:35 +0900

    포트폴리오 next 작업 - feature/각 영역 및 섹션 레이아웃 컨셉 작업
    1. 각 영역 레이아웃 처리
    - Html5 시맨틱 태그를 이용(header, nav, main, footer등)한 영역별 레이아웃
    - tailBlocks를 참고한 컨셉 UI 적용
    - 반응형 처리 확인
    
    2. 각 섹션별 레이아웃 처리
    - Html5 시맨틱 태그 section을 이용한 섹션별 레이아웃
    - tailBlocks를 참고한 컨셉 UI 적용
    - 반응형 처리 확인

---
+++ app/layout/contents.tsx
@@ -0,0 +1,15 @@
+import AboutMe from "../pages/aboutme";
+import Contacts from "../pages/contacts";
+import Projects from "../pages/projects";
+import Skills from "../pages/skills";
+
+export default function Contents() {
+  return (
+    <main>
+      <AboutMe></AboutMe>
+      <Skills></Skills>
+      <Projects></Projects>
+      <Contacts></Contacts>
+    </main>
+  );
+}
